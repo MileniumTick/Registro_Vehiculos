c8fc97d159d229b360b8b2720670870c2f1d3cd2
mileniumtick <josuechavarria98@gmail.com>
2022-11-13 17:48:47 -0600

    Bugs form 1

--- Registro_Vehiculos_App/Form1.Designer.vb
+++ Registro_Vehiculos_App/Form1.Designer.vb
@@ -24,7 +24,6 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Label3 = New System.Windows.Forms.Label()
@@ -48,15 +47,6 @@ Partial Class Form1
         Me.Label2.Size = New System.Drawing.Size(48, 16)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Label2"
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(107, 192)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(347, 40)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox1
         '
@@ -101,7 +91,6 @@ Partial Class Form1
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
@@ -113,40 +102,11 @@ Partial Class Form1
 
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-<<<<<<< HEAD
+
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button2 As Button
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-=======
-    Friend WithEvents CheckBox1 As CheckBox
->>>>>>> 119a2648b7d5a7eeb831e813daddd428f61b826f
-=======
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.SuspendLayout()
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(240, 204)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(391, 138)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Form1
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(935, 642)
-        Me.Controls.Add(Me.Button1)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
-        Me.ResumeLayout(False)
 
-    End Sub
-
-    Friend WithEvents Button1 As Button
->>>>>>> 3613b77eefd1a3bac39b35f0a6ad2e94c689fe74
 End Class
